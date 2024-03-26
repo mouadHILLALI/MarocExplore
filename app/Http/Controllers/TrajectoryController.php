99af2c070c133d154d2bd5e46cf46e0383a4b510
mouadHILLALI <mauadtoto001@gmail.com>
2024-03-26 15:09:57 +0000

    working on the backend

---
+++ app/Http/Controllers/TrajectoryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Trajectory;
+use Illuminate\Http\Request;
+
+class TrajectoryController extends Controller
+{
+   public function createRoute(Request $r){
+    try {
+        Trajectory::create([
+        'title' =>$r->title,
+        'start_date'  =>$r->start_date,
+        'end_date'  =>$r->end_date,
+        'image'  =>$r->image,
+        'user_id'  =>auth()->user()->id,
+        'category_id'  =>$r->category_id,
+        ]) ;
+        return response()->json([
+            'trajectory created successfully' 
+        ]) ;
+    } catch (\Throwable $th) {
+        return response()->json([
+            'unable to create trajectory' 
+        ]) ;
+    } 
+   }
+}
