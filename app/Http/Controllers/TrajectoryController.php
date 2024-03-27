c171594ac7350153c2511fc83cfcd3549ed96a5e
mouadHILLALI <mauadtoto001@gmail.com>
2024-03-27 10:04:04 +0000

    api endpoints

--- app/Http/Controllers/TrajectoryController.php
+++ app/Http/Controllers/TrajectoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class TrajectoryController extends Controller
 {
-   public function createRoute(Request $r){
+   public function createTrajectory(Request $r){
     try {
         Trajectory::create([
         'title' =>$r->title,
@@ -27,4 +27,23 @@ class TrajectoryController extends Controller
         ]) ;
     } 
    }
+   public function editTrajectory(Request $r)
+{
+    try {
+        $trajectory = Trajectory::findOrFail($r->trajectory_id);
+        $trajectory->update([
+            'title' => $r->title,
+            'start_date' => $r->start_date,
+            'end_date' => $r->end_date,
+            'image' => $r->image,
+            'user_id' => auth()->user()->id,
+            'category_id' => $r->category_id,
+        ]);
+
+        return response()->json('Trajectory updated successfully', 200);
+    } catch (\Throwable $th) {
+        return response()->json('Unable to update the trajectory', 500);
+    }
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('User/Register',[ProfileController::class , 'register']);
+Route::post('User/Login',[ProfileController::class , 'login']);
